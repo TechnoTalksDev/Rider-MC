850b97e14862c14d1b28d37a68ecd5acb75f7a49
TechnoTalksDev <technotalksyt101@gmail.com>
2022-02-24 10:50:29 -0800

    AH Forgot to push!
    This push is for the 1.2.0-beta release I forgot to push the release should still have the right jar though!

--- Rider/src/main/java/me/technotalks/ridermc/Main.java
+++ Rider/src/main/java/me/technotalks/ridermc/Main.java
@@ -8,6 +8,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 /*import org.bukkit.plugin.PluginDescriptionFile;*/
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
@@ -15,60 +16,66 @@ import java.io.IOException;
 public final class Main extends JavaPlugin implements Listener {
 
     public static Main instance = null;
-
+    //main handler
     @EventHandler
-    public void onInteract(PlayerInteractEntityEvent e) {
-        Player player = e.getPlayer();
-        boolean permission = this.getConfig().getBoolean("usePermission");
-        boolean requireSaddle= this.getConfig().getBoolean("requireSaddle");
-        boolean rideMessage = this.getConfig().getBoolean("sendRideMessage");
-        if (permission) {
-            if (player.hasPermission("rider.rideall")) {
-                if (requireSaddle) {
-                    if (!player.hasPermission("rider.bypass_saddle")) {
-                        if(player.getItemInHand().getType() == Material.SADDLE) {
+    public void onEntityInteract(PlayerInteractEntityEvent e) {
+        if (e.getHand()!=EquipmentSlot.HAND){
+            return;
+        }
+        else{
+            Player player = e.getPlayer();
+            boolean permission = this.getConfig().getBoolean("usePermission");
+            boolean requireSaddle= this.getConfig().getBoolean("requireSaddle");
+            boolean rideMessage = this.getConfig().getBoolean("sendRideMessage");
+            if (permission) {
+                if (player.hasPermission("rider.rideall")) {
+                    if (requireSaddle) {
+                        if (!player.hasPermission("rider.bypass_saddle")) {
+                            if(player.getInventory().getItemInMainHand().getType() == Material.SADDLE) {
+                                if(rideMessage) {
+                                    player.sendMessage(ChatColor.GOLD+"Enjoy your ride!");
+                                }
+                                e.getRightClicked().addPassenger(e.getPlayer());
+                            }else {
+                                player.sendMessage(ChatColor.RED+"You must have a saddle to ride!");
+                            }
+                        }else {
                             if(rideMessage) {
                                 player.sendMessage(ChatColor.GOLD+"Enjoy your ride!");
                             }
-                            e.getRightClicked().setPassenger(e.getPlayer());
-                        }else {
-                            player.sendMessage(ChatColor.RED+"You must have a saddle to ride!");
+                            e.getRightClicked().addPassenger(e.getPlayer());
                         }
                     }else {
                         if(rideMessage) {
                             player.sendMessage(ChatColor.GOLD+"Enjoy your ride!");
                         }
-                        e.getRightClicked().setPassenger(e.getPlayer());
+                        e.getRightClicked().addPassenger(e.getPlayer());
+                    }
+
+                }else {
+                    player.sendMessage(ChatColor.RED+"You don't have the required Permission! rider.rideall");
+                    player.sendMessage(ChatColor.RED+"Contact a Admin if you think this is a mistake!");
+                }
+            }else {
+                if (requireSaddle) {
+                    if(player.getInventory().getItemInMainHand().getType() == Material.SADDLE) {
+                        if(rideMessage) {
+                            player.sendMessage(ChatColor.GOLD+"Enjoy your ride!");
+                        }
+                        e.getRightClicked().addPassenger(e.getPlayer());
+                    }else{
+                        player.sendMessage(ChatColor.RED+"You must have a saddle to ride!");
+                        /*player.sendMessage("Equipment "+e.getRightClicked());*/
                     }
                 }else {
                     if(rideMessage) {
                         player.sendMessage(ChatColor.GOLD+"Enjoy your ride!");
                     }
-                    e.getRightClicked().setPassenger(e.getPlayer());
+                    e.getRightClicked().addPassenger(e.getPlayer());
                 }
+            }
 
-            }else {
-                player.sendMessage(ChatColor.RED+"You don't have the required Permission! rider.rideall");
-                player.sendMessage(ChatColor.RED+"Contact a Admin if you think this is a mistake!");
-            }
-        }else {
-            if (requireSaddle) {
-                if(player.getItemInHand().getType() == Material.SADDLE) {
-                    if(rideMessage) {
-                        player.sendMessage(ChatColor.GOLD+"Enjoy your ride!");
-                    }
-                    e.getRightClicked().setPassenger(e.getPlayer());
-                }else{
-                    player.sendMessage(ChatColor.RED+"You must have a saddle to ride!");
-                }
-            }else {
-                if(rideMessage) {
-                    player.sendMessage(ChatColor.GOLD+"Enjoy your ride!");
-                }
-                e.getRightClicked().setPassenger(e.getPlayer());
-            }
         }
-
 
     }
 
@@ -80,12 +87,13 @@ public final class Main extends JavaPlugin implements Listener {
                 getLogger().info(ChatColor.GOLD+"You are running the latest version of Rider!");
             } else {
                 getLogger().info(ChatColor.RED+"You are running version "+getDescription().getVersion()+"! The latest version is "+updateChecker.getLatestVersion()+"! Please update!");
+                getLogger().info(ChatColor.GOLD+"If the above message states that you are using a version 'ahead' please ignore it.");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
         Bukkit.getPluginManager().registerEvents(this, this);
-        System.out.println(ChatColor.GOLD+"Rider has loaded");
+        getLogger().info(ChatColor.GOLD+"Rider has loaded");
         this.getConfig().options().copyDefaults();
         saveDefaultConfig();
 
@@ -94,7 +102,7 @@ public final class Main extends JavaPlugin implements Listener {
     @Override
     public void onDisable() {
 
-        System.out.println(ChatColor.GOLD+"Rider has unloaded!");
+        getLogger().info(ChatColor.GOLD + "Rider has unloaded!");
 
     }
 }
